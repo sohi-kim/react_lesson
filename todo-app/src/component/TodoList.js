cce353e726e130f4b0bab48bbde76714415049c6
--global <soh.kim.veron@gmail.com>
2024-12-20 14:52:02 +0900

    day4 todo 완료

--- todo-app/src/component/TodoList.js
+++ todo-app/src/component/TodoList.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './TodoList.scss'
 import TodoListItem from './TodoListItem';
 
-function TodoList({todos,onRemove,onChecked,onTest}) {
+function TodoList({todos,onRemove,onChecked}) {
   return (
     <div className='TodoList'>
       {/* ListItem을 반복해서 표시 */}
       {/* 항목 삭제 아이콘 TodoListItem 컴포넌트에 있음. */}
       {todos.map( (item) =>
         <TodoListItem todo={item} onChecked={onChecked}
-              onRemove={onRemove} key={item.id} onTest={onTest}/>
+              onRemove={onRemove} key={item.id} />
       )}
     </div>
   );
